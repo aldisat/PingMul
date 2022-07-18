564a8b3e2f5433a15090036a2eaac2dc9a2bbce9
Aldi Satria <52058660+acvn@users.noreply.github.com>
2022-07-18 17:13:50 +0700

    asd

--- pingmul.py
+++ pingmul.py
@@ -1,10 +1,22 @@
 import os
-hostname = "google.com" #example
-response = os.system("ping -c 1 " + hostname)
+import platform
 
-#and then check the response...
-if response == 0:
-    print(hostname, 'is up!')
-else:
-    print(hostname, 'is down!')
+with open("ip_list.txt") as file:
+    park = file.read()
+    park = park.splitlines()
+    print(f" {park}  \n")
 
+param = "-n" if platform.system().lower()=="windows" else "-c"
+kumpul = ""
+
+for ip in park:
+    response = os.system("ping " + param + " 1 " + ip)
+
+    if response == 0:
+        print(response)
+        kumpul += ip + " is up!\n"
+    else:
+        print(response)
+        kumpul += ip + " is down!\n"
+
+print(kumpul)
